2aabe1df52f8b3c5b379687c5ae44090663c8dd9
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-06-22 21:30:01 +0530

    feat: add shadcn UI, theme, button component and utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error type error without @types/node package
 import process from "node:process";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
