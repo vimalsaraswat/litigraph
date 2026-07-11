c17ccfd6f6c7e1b0d27dd043de841348730cd45f
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-11 22:29:11 +0530

    refactor: remove unused position and property domain modules

--- src-tauri/src/domain/mod.rs
+++ src-tauri/src/domain/mod.rs
@@ -1,12 +1,8 @@
 pub mod entity;
-pub mod position;
-pub mod property;
 pub mod relationship;
 pub mod workspace;
 
 pub use entity::{Entity, EntityId, EntityType};
-pub use position::Position;
-pub use property::Property;
 pub use relationship::{Relationship, RelationshipId, RelationshipType};
 pub use workspace::{Workspace, WorkspaceId};
 
