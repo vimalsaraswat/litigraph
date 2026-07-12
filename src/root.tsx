e45f414ff53bd2ca8fef410b7ff22e047caf01d6
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-12 22:02:04 +0530

    chore: update document title to LitiGraph

--- src/root.tsx
+++ src/root.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tauri + React + Typescript</title>
+        <title>LitiGraph</title>
         <Meta />
         <Links />
       </head>
