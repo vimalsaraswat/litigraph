c3158467db512303737439d46cf79b944de7b55b
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-13 23:12:21 +0530

    refactor: simplify domain module exports

--- src-tauri/src/domain/mod.rs
+++ src-tauri/src/domain/mod.rs
@@ -1,19 +1,3 @@
-pub mod entity;
-pub mod relationship;
 pub mod workspace;
 
-pub use entity::{Entity, EntityId, EntityType};
-pub use relationship::{Relationship, RelationshipId, RelationshipType};
-pub use workspace::{Workspace, WorkspaceId};
-
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
-pub struct Property {
-    pub key: String,
-    pub value: String,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, serde::Serialize, serde::Deserialize)]
-pub struct Position {
-    pub x: f64,
-    pub y: f64,
-}
+pub use workspace::*;
